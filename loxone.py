2e2d0b01858cf94764625b7b6a087a1852194f7c
Tomas Klouda <t.klouda@email.cz>
2017-06-03 20:28:01 +0200

    Move http connection logic to loxone client library
    Make it possible to use all three libs for http connection:
    urllib2
    urllib3
    requests

--- loxone.py
+++ loxone.py
@@ -5,6 +5,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import urllib2
 from base64 import b64encode
+import urllib3
 
 import json
 
@@ -43,6 +44,22 @@ def get_url2(url, user, password):
         return None
 
 
+def get_url3(url, user, password):
+    try:
+        http = urllib3.PoolManager()
+        headers = urllib3.util.make_headers(basic_auth=user + ':' + password)
+        response = http.request('GET', url, headers = headers)
+    except urllib3.exceptions.NewConnectionError as e:
+        lg.error("Connection to Miniserver failed with: {}".format(e))
+        return None
+
+    if (response.status == 200 ):
+        return json.loads(response.data)
+    else:
+        lg.error("Connection to Miniserver failed with http status code: {}".format(response.status))
+        return None
+
+
 def strip_units(value):
     while value:
         try:
@@ -60,16 +77,23 @@ def loxclient(host, user, password, action='state', obj=None, strip=False, lib='
     else:
         url = "http://{0}/jdev/sps/io/{2}/{1}".format(host, action, obj)
     lg.debug("Loxone URL is: {}".format(url))
+
     # Get Json data from Miniserver
     #mock_data = str('{"LL": { "control": "dev/sps/io/LightSensor_Pracovna/state", "value": "531.68", "Code": "200"}}')
     #jData = json.loads(mock_data)
-    #jData = get_req(url, user, password)
-    get = {'requests': get_req, 'urllib2': get_url2}
+    get = {'requests': get_req, 'urllib2': get_url2, 'urllib3': get_url3}
     jData = get[lib](url, user, password)
+    if jData == None:
+        # Getting the data failed, bailing out
+        return None
     lg.debug("The response json content is: {}".format(jData))
+
+    # Check return code inside the json. If it's not 200, response probably doesn't contain what we asked for.
     if jData['LL']['Code'] != '200':
         lg.error("Miniserver returned error code: {}".format(jData['LL']['Code']))
         return None
+
+    # Get the value from json response and optionally strip it from units.
     value = jData['LL']['value']
     if strip:
         value = float(strip_units(value))
