c874fa28fcfea1d1ea1f573fc373d187ff5e1e36
Tomas Klouda <t.klouda@email.cz>
2017-06-10 09:51:51 +0200

    Refactor logger
    Add commandline arguments parsing
    Add dry run and http lib options for perf tunning
    Improve logging configuration
    Move code out of main while loop into functions

--- logger.py
+++ logger.py
@@ -4,6 +4,7 @@
 import time
 import sys
 import datetime
+import argparse
 
 # import InfluxDB client
 from influxdb import InfluxDBClient
@@ -12,13 +13,37 @@ from influxdb import InfluxDBClient
 from config import load_config, load_measurements
 from loxone import loxclient
 
+parser = argparse.ArgumentParser(description="Logger script for getting stats from Miniserver to InfluxDB")
+parser.add_argument("-v", "--verbose", action = "store_true",
+                    help="Enable verbose output")
+parser.add_argument("-d", "--debug", action = "store_true",
+                    help="Enable debug output")
+parser.add_argument("--dry", action = "store_true",
+                    help="Dry-run. No writing to InfluxDB. Only get values from Miniserver and time it.")
+# Allow user to set session and run number via args otherwise auto-generate
+parser.add_argument("-s", "--session", default = "dev",
+                    help="Logging session. Default is ''dev'")
+parser.add_argument("-r", "--run", default = None,
+                    help="Run number. Default is generated from datetime.now()")
+parser.add_argument("-l", "--lib", default = "requests",
+                    help="Http library. Default is 'requests'")
+args = parser.parse_args()
+
 # Initialize logging
 import logging
 lg = logging.getLogger(__name__)
+if args.verbose:
+    #print("Verbose output turned on")
+    loglvl = logging.INFO
+elif args.debug:
+    #print("Debug output turned on")
+    loglvl = logging.DEBUG
+else:
+    loglvl = logging.ERROR
 logging.basicConfig(
     # filename=args.log_file,
     format='%(asctime)s %(threadName)s [%(levelname)s] %(name)s %(message)s',
-    level=logging.DEBUG
+    level=loglvl
     )
 
 # Read configuration from config file
@@ -35,6 +60,16 @@ loxhost = config_cache["loxone::host"]
 # Sample period (s)
 interval = 60
 
+# Allow user to set session and run number via args otherwise auto-generate
+if args.run is None:
+    now = datetime.datetime.now()
+    runNo = now.strftime("%Y%m%d%H%M")
+else:
+    runNo = args.run
+session = args.session
+lg.debug("Session: {}".format(args.session))
+lg.debug("Run number: {}".format(runNo))
+
 
 def wait(n):
     '''Wait until the next increment of n seconds'''
@@ -42,58 +77,58 @@ def wait(n):
     lg.debug("It is {0} now, will wait {1}s with next execution".format(time.asctime(), n - (x % n)))
     time.sleep(n - (x % n))
 
-# Allow user to set session and run number via args otherwise auto-generate
-# TODO remove this section once we get out of alfa testing stage
-if len(sys.argv) > 1:
-    if (len(sys.argv) < 3):
-        print("Must define session and run number!!")
-        sys.exit()
-    else:
-        session = sys.argv[1]
-        runNo = sys.argv[2]
-else:
-    session = "dev"
-    now = datetime.datetime.now()
-    runNo = now.strftime("%Y%m%d%H%M")
 
-lg.debug("Session: {}".format(session))
-lg.debug("runNo: {}".format(runNo))
+def get_measurements():
+    measurements = {}
+    # Get all measurements from Miniserver
+    lg.info("Getting measurements from Miniserver...")
+    for loxobject in load_measurements('measurements.txt'):
+        loxval = loxclient(loxhost, loxusr, loxpass, obj=loxobject, strip=True, lib=args.lib)
+        if loxval is None:
+            lg.error("Getting of value for {} failed.".format(loxobject))
+            continue
+        measurements[loxobject] = loxval
+        lg.debug("{0} = {1}".format(loxobject, loxval))
+        #lg.debug("Obtained measurements: {}".format(measurements))
+    lg.info("Obtained measurements: {}".format(measurements))
+    return measurements
 
 # Create the InfluxDB object
 client = InfluxDBClient(host, port, user, password, dbname)
+
+
+def write_measurements(data):
+    json_body = [
+        {
+            "measurement":
+                session,
+                "tags": {"run": runNo},
+                "time": iso,
+                "fields": data
+        }
+    ]
+
+    # Write JSON to InfluxDB
+    lg.debug("The write json content is: {}".format(json_body))
+    client.write_points(json_body)
+
+if args.dry:
+    from timeit import Timer
+    t_collect = Timer("get_measurements()", "from __main__ import get_measurements")
+    print 'Time to get all measurements: {0}'.format(t_collect.timeit(number=1))
+    exit(0)
 
 # Run until keyboard out
 try:
     while True:
         iso = time.ctime()
-        measurements = {}
-        # Get all measurements from Miniserver
-        for loxobject in load_measurements('measurements.txt'):
-            loxval = loxclient(loxhost, loxusr, loxpass, obj=loxobject, strip=True)
-            if loxval is None:
-                lg.error("Getting of value for {} failed.".format(loxobject))
-                continue
-            measurements[loxobject] = loxval
+        measurements = get_measurements()
         if measurements == {}:
             lg.error("We didn't receive any values from Miniserver. Not going to push anything to InfluxDB.")
         else:
-            lg.debug("Obtained measurements: {}".format(measurements))
-            json_body = [
-                {
-                    "measurement":
-                        session,
-                        "tags": {"run": runNo},
-                        "time": iso,
-                        "fields": measurements
-                  }
-                ]
-
-            # Write JSON to InfluxDB
-            lg.debug("The write json content is: {}".format(json_body))
-            client.write_points(json_body)
+            write_measurements(measurements)
         # Wait for next sample
-        # time.sleep(interval)
-        wait(60)
+        wait(interval)
 
 except KeyboardInterrupt:
     pass
